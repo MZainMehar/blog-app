d985c4401b114a890bd2ec0064f6734f5708cd0b
MZainMehar <mzainmehar-developer@outlook.com>
2024-08-06 10:32:29 +0500

    Add API routes for creating, updating, and deleting topics

---
+++ app/api/topics/[id]/route.js
@@ -0,0 +1,11 @@
+import connectMongoDB from "@/libs/mongodb";
+import Topic from "@/models/topic";
+import { NextResponse } from "next/server";
+
+export async function PUT(req, {params}) {
+    const { id } = params;
+    const { title, description } = await req.json();
+    await connectMongoDB();
+    await Topic.findByIdAndUpdate(id, { title, description });
+    return NextResponse.json({ message: "Topic updated successfully" }, { status: 200 });
+}
